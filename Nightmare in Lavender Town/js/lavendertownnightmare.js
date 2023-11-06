ae8c2a587f57b808e4bd2f75a97e1f6506571627
Antonio Cordero Molina <acormol2001@g.educaand.es>
2023-11-06 23:32:17 +0100

    nuevo ajercicio añadido

---
+++ Nightmare in Lavender Town/js/lavendertownnightmare.js
@@ -0,0 +1,16 @@
+/*********************************************************
+ * js functions for Nightmare Before Crhistmas exercise
+ * author: Antonio Cordero Molina
+ * version: 1.0
+ * date november 2023
+ * comment: my first exercise with JavaScript
+ * *******************************************************/
+
+/*
+function show() {
+
+}
+
+function hide() {
+    
+}*/
